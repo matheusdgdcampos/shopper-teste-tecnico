d0dfa1d256cd5a24e5102b38342d78f3b94ef989
Matheus Campos <matheus.rj.ig@gmail.com>
2024-08-30 20:10:11 -0300

    feat: config validation error exceptions and increase payload size

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from './config/environment-validation';
+import { json, urlencoded } from 'express';
+import { ValidationError } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
       forbidNonWhitelisted: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        const result = errors.map((error) => ({
+          property: error.property,
+          message: error.constraints[Object.keys(error.constraints)[0]],
+        }));
+
+        return new BadRequestException({
+          error_code: 'INVALID_DATA',
+          error_description: result,
+        });
+      },
     }),
   );
-  await app.listen(3000);
+  const configService = app.get(ConfigService<EnvironmentVariables>);
+  const logger = new Logger(bootstrap.name);
+  await app.listen(configService.getOrThrow('PORT', { infer: true }), () => {
+    logger.log('Server is running!');
+  });
 }
 bootstrap();
